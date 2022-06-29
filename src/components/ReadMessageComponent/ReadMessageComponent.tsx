3ccfeba01f2ca753fc14c3750e49aee6234b05e3
Jan <jan.blaszczyk@protonmail.com>
2022-06-29 20:18:23 +0200

    Sending and reading messages fully functional from FE

--- src/components/ReadMessageComponent/ReadMessageComponent.tsx
+++ src/components/ReadMessageComponent/ReadMessageComponent.tsx
@@ -1,34 +1,58 @@
 import React, {SyntheticEvent, useState} from 'react';
 import {Btn} from "../common/Btn";
+import {OneMessageFromDB} from "types";
 
 import './ReadMessageComponent.css';
 
 export const ReadMessageComponent = () => {
 
-    const [secretKey, setSecretKey] = useState({
+    const CREDENTIALS = {
         secretKey: '',
         sender: ''
-    });
+    }
+    const SECRET_MESSAGE: OneMessageFromDB ={
+        sender: '',
+        secretKey: '',
+        body: '',
+        createdAt: ''
+    }
+
+
+    const [credentials, setCredentials] = useState(CREDENTIALS);
+    const [secretMessage, setSecretMessage] = useState(SECRET_MESSAGE)
 
     const readMessage = async (e: SyntheticEvent) => {
         e.preventDefault();
 
+        try {
+            const res = await fetch(`http://localhost:3001/message/${credentials.sender}/${credentials.secretKey}`);
+            const data: OneMessageFromDB = await res.json();
+            setSecretMessage({
+                ...data
+            })
+            
+        } finally {
+            console.log('secretMessage',secretMessage); // TODO ogarnac ten temat
+            
+        }
+        setCredentials(CREDENTIALS);
     };
 
     const updateSecretKey = (key: string, value: any) => {
-        setSecretKey(secret => ({
+        setCredentials(secret => ({
             ...secret,
             [key]: value,
         }));
     };
-
 
     return (
         <>
         <div className="show-message">
             <h3>Here you will see the secret message:</h3>
             <br/>
-            <p>SMS</p>
+            <p>Message from: {secretMessage.sender}</p>
+            <p>Sent on: {secretMessage.createdAt}</p>
+            <p>Message body: {secretMessage.body}</p>
         </div>
         <form action="" className="send-form" onSubmit={readMessage}>
             <h3>Read secret message</h3>
--- src/components/SendMessageComponent/SendMessageComponent.tsx
+++ src/components/SendMessageComponent/SendMessageComponent.tsx
@@ -1,5 +1,6 @@
 import React, {SyntheticEvent, useState} from 'react';
 import {Btn} from "../common/Btn";
+import {CreateMessageApiResponse} from "types";
 
 import './SendMessageComponent.css';
 
@@ -11,17 +12,41 @@ export const SendMessageComponent = () => {
         body: '',
         toBeDeletedAfterRead: false
     }
+    const APIRES: CreateMessageApiResponse = {
+        isSucces: false,
+        secretKey: '',
+        sender: '',
+        errMsg: ''
+    }
     const MAX_SENDER_LENGTH: number = 30
     const MAX_MESSAGE_LENGTH: number = 500
 
     const [message, setMessage] = useState(MSG);
+    const [apiResponse, setApiResponse] = useState(APIRES)
 
     const saveMessage = async (e: SyntheticEvent) => {
         e.preventDefault();
-        console.log(message)
-
-
-        setMessage(MSG)
+        
+        try {
+            const res = await fetch(`http://localhost:3001/message/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    ...message
+                }),
+            });
+            const data: CreateMessageApiResponse = await res.json();
+            setApiResponse({
+                ...data
+            })
+            
+        } finally {
+            console.log('wynik',apiResponse); // TODO ogarnac ten temat
+            setMessage(MSG);
+        }
+        console.log('wynik',apiResponse);
     };
 
     const updateForm = (key: string, value: any) => {
@@ -33,47 +58,51 @@ export const SendMessageComponent = () => {
 
 
     return (
-        <form action="" className="send-form" onSubmit={saveMessage}>
-            <h2>Create secret message</h2>
-            <p>
-                <label>
-                    Sender: <br/><br/>
-                    <input
-                        type="text"
-                        title="sender"
-                        required
-                        maxLength={MAX_SENDER_LENGTH}
-                        value={message.sender}
-                        onChange={e => updateForm('sender', e.target.value)}
-                    />
-                </label>
-            </p>
-            <p>
-                <label>
-                    Message: <br/><br/>
-                    <textarea
-                        className='message-body'
-                        name="description"
-                        maxLength={MAX_MESSAGE_LENGTH}
-                        value={message.body}
-                        onChange={e => updateForm('body', e.target.value)}
-                    />
-                </label>
-            </p>
-            <p>
-                <label>
-                    <input 
-                        type="checkbox" 
-                        name="toBeDeletedAfterRead" 
-                        id="" 
-                        onChange={e => updateForm('toBeDeletedAfterRead', e.target.checked)}
-                    />
-                    Delete message after 24h*?
-                </label>
-            </p>
-            <Btn text="Send message"/>
+        <>
+            
+            <form action="" className="send-form" onSubmit={saveMessage}>
+                <p>{apiResponse.isSucces ?? `Dear ${apiResponse.sender}. Your secret key is ${apiResponse.secretKey}. Pass these credentials to the person who you sent this message to as this is the first and last time where the secret key is visible for you`}</p>
+                <h2>Create secret message</h2>
+                <p>
+                    <label>
+                        Sender: <br/><br/>
+                        <input
+                            type="text"
+                            title="sender"
+                            required
+                            maxLength={MAX_SENDER_LENGTH}
+                            value={message.sender}
+                            onChange={e => updateForm('sender', e.target.value)}
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message: <br/><br/>
+                        <textarea
+                            className='message-body'
+                            name="description"
+                            maxLength={MAX_MESSAGE_LENGTH}
+                            value={message.body}
+                            onChange={e => updateForm('body', e.target.value)}
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <input 
+                            type="checkbox" 
+                            name="toBeDeletedAfterRead" 
+                            id="" 
+                            onChange={e => updateForm('toBeDeletedAfterRead', e.target.checked)}
+                        />
+                        Delete message after 24h*?
+                    </label>
+                </p>
+                <Btn text="Send message"/>
 
-            <p><small>* The message will be deleted after 24h regardless whether it will be read or not</small></p>
-        </form>
+                <p><small>* The message will be deleted after 24h regardless whether it will be read or not</small></p>
+            </form>
+        </>
     );
 }
